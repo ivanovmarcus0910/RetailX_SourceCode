dd9c296a24fdb1cbaacea9056ad865535527d9ea
nguyenlamhai <hainlde180364@fpt.edu.vn>
2025-11-18 17:34:28 +0700

    Products

---
+++ DataAccessObject/ProductDAO.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessObject
+{
+    public class ProductDAO
+    {
+        private readonly Tenant0Context _context;
+
+        public ProductDAO(Tenant0Context context)
+        {
+            _context = context;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Supplier)
+                .ToList();
+        }
+
+        public Product? GetProductById(int id)
+        {
+            return _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Supplier)
+                .FirstOrDefault(p => p.ProductId == id);
+        }
+
+        public void AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void DeleteProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject.Models;
+using DataAccessObject;
+
+namespace Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductDAO _dao;
+
+        public ProductRepository(Tenant0Context context)
+        {
+            _dao = new ProductDAO(context);
+        }
+
+        public List<Product> GetAll() => _dao.GetAllProducts();
+
+        public Product? GetById(int id) => _dao.GetProductById(id);
+
+        public void Insert(Product product) => _dao.AddProduct(product);
+
+        public void Update(Product product) => _dao.UpdateProduct(product);
+
+        public void Delete(int id) => _dao.DeleteProduct(id);
+    }
+}
