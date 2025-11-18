8969a2521f5d445a75a7c5eabfde980c474aa613
trinhquoctrung <trungtqde180411@fpt.edu.vn>
2025-11-18 18:34:31 +0700

    function manager salary

---
+++ DataAccessObject/StaffDAO.cs
@@ -0,0 +1,32 @@
+﻿using BusinessObject.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccessObject
+{
+    public class StaffDAO
+    {
+        private readonly Tenant0Context _context;
+
+        public StaffDAO(Tenant0Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Staff>> GetAllActiveStaffs()
+        {
+            return await _context.Staff
+                                 .Where(s => s.BaseSalary.HasValue)
+                                 .ToListAsync();
+        }
+
+        public async Task<Staff> GetStaffById(int staffId)
+        {
+            // Lấy thông tin nhân viên theo ID
+            return await _context.Staff
+                                 .FirstOrDefaultAsync(s => s.StaffId == staffId);
+        }
+    }
+}
